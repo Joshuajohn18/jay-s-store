4780204f4ab4ec27d93b7e62cfa9da5c4b472bda
joshuajohn <felexagent9@gmail.com>
2025-08-03 05:12:30 -0700

    Updated show.html and template responsiveness

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
  path('', views.index, name='products'),
- path('product/<int:id>/', views.show_product, name='show_product')
+ path('product/<int:id>/', views.show, name='show_product')
  ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,25 @@
 # products/views.py
 from django.shortcuts import render, get_object_or_404
 from .models import Product
-
 def index(request):
-    products = Product.objects.all()
-    return render(request, 'products/index.html', {'products': products})
+    search_term = request.GET.get('search')
+    
+    if search_term:
+        products = Product.objects.filter(name__icontains=search_term)
+    else:
+        products = Product.objects.all()
+    
+    template_data = {
+        'title': 'products',
+        'products': products
+    }
 
-def show_product(request, id):
-    product = get_object_or_404(Product, id=id)
-    return render(request, 'products/show.html', {'product': product})
+    return render(request, 'products/index.html', {'template_data': template_data})
+
+def show(request, id):
+      product =  get_object_or_404(Product,id=id)
+      template_data= {}
+      template_data['title'] = product.name
+      template_data['product'] = product
+      return render (request, 'products/show.html',
+ {'template_data': template_data})
